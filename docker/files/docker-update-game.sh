ce7a5f19453a29338082ddb2c15d891aa5b7e5d9
panzerjet <michael27siu@hotmail.com>
2023-06-18 15:48:01 +0800

    fix line continuation

--- docker/files/docker-update-game.sh
+++ docker/files/docker-update-game.sh
@@ -2,8 +2,8 @@
 set -eou pipefail
 
 # Update installation files
-ARCHIVE="/tmp/factorio_headless_x64_$1.tar.xz" \
-curl -sSL "https://www.factorio.com/get-download/$1/headless/linux64" -o "$ARCHIVE" --retry 8 \
+ARCHIVE="/tmp/factorio_headless_x64_$1.tar.xz"
+curl -sSL "https://www.factorio.com/get-download/$1/headless/linux64" -o "$ARCHIVE" --retry 8
 
 # Remove old files except 
 find "$FACTORIO_APP" -mindepth 1 -type d \
@@ -13,5 +13,5 @@ find "$FACTORIO_APP" -mindepth 1 -type d \
 -exec rm -r {} \;
 
 # Inflate binaries
-tar -xf "$ARCHIVE" --directory /opt \
+tar -xf "$ARCHIVE" --directory "$FACTORIO_APP/.."
 rm "$ARCHIVE"
