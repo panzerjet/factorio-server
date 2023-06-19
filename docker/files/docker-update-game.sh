3309f0f81fa8220c78183a30075b684855cc116b
panzerjet <michael27siu@hotmail.com>
2023-06-19 20:47:24 +0800

    reformat

--- docker/files/docker-update-game.sh
+++ docker/files/docker-update-game.sh
@@ -7,7 +7,11 @@ ARCHIVE="/tmp/factorio_headless_x64_$1.tar.xz"
 curl -sSL "https://www.factorio.com/get-download/$1/headless/linux64" -o "$ARCHIVE" --retry 8
 
 # Remove old files except 
-find $FACTORIO_APP/* ! -path "$FACTORIO_APP/scenarios*" ! -path "$FACTORIO_APP/saves*" ! -path "$FACTORIO_APP/config" ! -path "$FACTORIO_APP/config/*" -delete
+find $FACTORIO_APP/* \ 
+    ! -path "$FACTORIO_APP/scenarios*" \
+    ! -path "$FACTORIO_APP/saves*" \
+    ! -path "$FACTORIO_APP/config" \
+    ! -path "$FACTORIO_APP/config/*" -delete
 
 # Inflate binaries
 tar -xf "$ARCHIVE" --directory "$FACTORIO_APP/.."
